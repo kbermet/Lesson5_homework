814ccfaed66a7f259aa6521cbe2f936f38b95924
kbermet <kbermet@gmail.com>
2021-02-07 15:22:52 +0600

    All tasks completed.

--- homework_5.7.py
+++ homework_5.7.py
@@ -5,9 +5,13 @@
 # убытки, в расчет средней прибыли ее не включать.
 # Далее реализовать список. Он должен содержать словарь с фирмами и их прибылями, а также словарь со средней прибылью.
 # Если фирма получила убытки, также добавить ее в словарь (со значением убытков).
-# Пример списка: [{“firm_1”: 5000, “firm_2”: 3000, “firm_3”: 1000}, {“average_profit”: 2000}].
-# Итоговый список сохранить в виде json-объекта в соответствующий файл.
-# Пример json-объекта:
-# [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
-#
-# Подсказка: использовать менеджеры контекста.
+
+
+import json
+
+with open ('my_ex7.json', 'w', encoding='utf-8') as write_f:
+    with open ('text_7.txt', encoding="utf-8") as f_obj:
+        profit = {line.split()[0]: int(line.split()[2])- int(line.split()[3]) for line in f_obj}
+        result = [profit, {"average_profit": round(sum([int(i) for i in profit.values() if int(i)>0])/
+                                                   len([int(i) for i in profit.values() if int(i)>0]))}]
+        json.dump(result, write_f, ensure_ascii=False, indent=4)
